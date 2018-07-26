28a4de781beb9f90e23dab1e9b5a1c84ae813072
jakesboy2 <jcwaldrip@att.net>
2018-07-26 01:08:38 -0500

    added creep role functions (boiler plate)

--- prototype.flags.js
+++ prototype.flags.js
@@ -1,1 +1,42 @@
-//
+'use strict';
+
+
+//sets the flag into a room memory based on the closest owned room to it
+Flag.prototype.assignRemoteFlagToRoom = function () {
+        
+}
+//----------------
+
+
+//sets claim flag to memory based on closest room to it
+Flag.prototype.assignClaimFlagToRoom = function () {
+    
+}
+//--------
+
+
+//sets attack flag to memory based on closest room to it
+Flag.prototype.assignAttackFlagToRoom = function () {
+	
+}
+//------
+
+
+Flag.prototype.assignFlagToRoom = function () {
+	
+	//if its double yellow, remote flags
+        if(this.color === COLOR_YELLOW && this.secondaryColor === COLOR_YELLOW) {
+            this.assignRemoteFlagToRoom();
+        }
+
+        //if its double white, claim flag
+        if(this.color === COLOR_WHITE && this.secondaryColor === COLOR_WHITE) {
+            this.assignClaimFlagToRoom();
+        }
+
+		//if its double red, attack flag
+        if(this.color === COLOR_RED && this.secondaryColor === COLOR_RED) {
+            this.assignAttackFlagToRoom();
+        }
+}
+//-----
--- role.Harvester.js
+++ role.Harvester.js
@@ -1,1 +1,61 @@
+'use strict';
 
+
+module.exports = {
+    run: function(creep) {
+		
+		var state;
+
+		//if no state, set state to spawning
+		if(creep.memory.state != undefined)
+		{	
+			state = creep.memory.state
+		}
+		else
+		{
+			creep.memory.state = 'STATE_SPAWNING';
+			state = creep.memory.state;
+		}
+		//--------
+
+
+		//check the state and act appropirately
+		switch(state)
+		{
+			case 'STATE_SPAWNING':
+
+
+
+			break;
+
+
+			case 'STATE_MOVING':
+
+
+
+			break;
+
+
+			case 'STATE_GET_RESOURCES':
+
+
+
+			break;
+
+
+			case 'STATE_USE_RESOURCES':
+
+
+
+			break;
+
+
+			default: 
+
+				console.log("Invalid creep state for " + creep.name);
+
+			break;
+		}
+    }
+    //---------------------
+};
