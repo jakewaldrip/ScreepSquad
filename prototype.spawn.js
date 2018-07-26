a57d747dcf88d1b789bc70e074c4b04bed83deec
UhmBrock <uhmbrock@hotmail.com>
2018-07-25 23:58:32 -0500

    #Bugfix Removed semicolons that were throwing errors.
    Signed-off-by: UhmBrock <uhmbrock@hotmail.com>

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -25,7 +25,7 @@ StructureSpawn.prototype.createMiner = function (homeRoom, energyCost) {
 		role: 'miner',
 		homeRoom: homeRoom,
 		state: 'STATE_SPAWNING',
-		workTarget: 'none';
+		workTarget: 'none'
 	}});
 }
 //----
@@ -58,7 +58,7 @@ StructureSpawn.prototype.createHarvester = function (homeRoom, energyCost) {
 		role: 'harvester',
 		homeRoom: homeRoom,
 		state: 'STATE_SPAWNING',
-		workTarget: 'none';
+		workTarget: 'none'
 	}});
 }
 //----
@@ -108,7 +108,7 @@ StructureSpawn.prototype.createWorker = function (homeRoom, energyCost) {
 		role: 'worker',
 		homeRoom: homeRoom,
 		state: 'STATE_SPAWNING',
-		workTarget: 'none';
+		workTarget: 'none'
 	}});
 }
 //----
