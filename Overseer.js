3b89f16f51d8fb06435523a7c2d31fe17e76593b
Jacob Waldrip <jcwaldrip@att.net>
2018-07-31 21:58:57 -0500

    boiler plate for overseer

--- Overseer.js
+++ Overseer.js
@@ -1,6 +1,9 @@
 module.exports = {
-    //I think I want to play around with making an Overseer object/class
     
-    // with properties like Overseer.room, Overseer.minions/creeps kind of thing.
+    //accept room as param and run the room
+    runRoom: function()
+    {
     
+    }
+
 }
