5f0a39d3f4d61a135250cc20632ffa120909434c
jakesboy2 <jcwaldrip@att.net>
2018-08-09 14:31:50 -0500

    added flag handling for claim and attack rooms

--- prototype.flags.js
+++ prototype.flags.js
@@ -64,6 +64,58 @@ Flag.prototype.assignRemoteFlagToRoom = function () {
 //sets claim flag to memory based on closest room to it
 Flag.prototype.assignClaimFlagToRoom = function () {
     
+    //loop through owned rooms and check if a this flag's room exists within its memory
+    var flagRoom = this.pos.roomName;
+    var ownedRooms = _.filter(Game.rooms, r => r.isOwnedRoom() == true)
+    var isExist = false;
+    
+    let currentRoom;
+    
+    for(let room in ownedRooms)
+    {   
+        currentRoom = ownedRooms[room];
+        var remoteRooms = Object.keys(currentRoom.memory.remoteRooms); 
+        
+        //check if the flag is assigned to this room and trigger the isExist bool and break the loop
+        if(_.some(remoteRooms, roomName => roomName == flagRoom))
+        {
+            isExist = true;
+            break;
+        }
+    }
+    
+
+    //if flag is not assigned to a room, find closest room and assign it
+    if(!isExist)
+    {
+        
+        let closestRoom = ownedRooms[0].name;
+        let bestDistance = Game.map.getRoomLinearDistance(flagRoom, closestRoom);
+        
+        let currentDistance;
+        
+        //loop over all rooms and flag the closest one
+        for(let room in ownedRooms)
+        {
+            //get the distance between the flag room and the room we're checking
+            currentDistance = Game.map.getRoomLinearDistance(flagRoom, ownedRooms[room].name);
+
+            //check if this distance is less than the previous distance
+            if(currentDistance < bestDistance)
+            {
+                closestRoom = ownedRooms[room].name;    
+                bestDistance = currentDistance;
+            }
+        }
+
+        //return the closest room to the remote room
+        console.log("Claim Room added to room " + closestRoom);
+        return closestRoom;
+    }
+	else
+	{
+		return null;
+	}
 }
 //--------
 
@@ -71,6 +123,58 @@ Flag.prototype.assignClaimFlagToRoom = function () {
 //sets attack flag to memory based on closest room to it
 Flag.prototype.assignAttackFlagToRoom = function () {
 	
+    //loop through owned rooms and check if a this flag's room exists within its memory
+    var flagRoom = this.pos.roomName;
+    var ownedRooms = _.filter(Game.rooms, r => r.isOwnedRoom() == true)
+    var isExist = false;
+    
+    let currentRoom;
+    
+    for(let room in ownedRooms)
+    {   
+        currentRoom = ownedRooms[room];
+        var remoteRooms = Object.keys(currentRoom.memory.remoteRooms); 
+        
+        //check if the flag is assigned to this room and trigger the isExist bool and break the loop
+        if(_.some(remoteRooms, roomName => roomName == flagRoom))
+        {
+            isExist = true;
+            break;
+        }
+    }
+    
+
+    //if flag is not assigned to a room, find closest room and assign it
+    if(!isExist)
+    {
+        
+        let closestRoom = ownedRooms[0].name;
+        let bestDistance = Game.map.getRoomLinearDistance(flagRoom, closestRoom);
+        
+        let currentDistance;
+        
+        //loop over all rooms and flag the closest one
+        for(let room in ownedRooms)
+        {
+            //get the distance between the flag room and the room we're checking
+            currentDistance = Game.map.getRoomLinearDistance(flagRoom, ownedRooms[room].name);
+
+            //check if this distance is less than the previous distance
+            if(currentDistance < bestDistance)
+            {
+                closestRoom = ownedRooms[room].name;    
+                bestDistance = currentDistance;
+            }
+        }
+
+        //return the closest room to the remote room
+        console.log("Attack Room added to room " + closestRoom);
+        return closestRoom;
+    }
+	else
+	{
+		return null;
+	}
 }
 //------
 
