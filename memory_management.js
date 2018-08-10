8f9bb9eed0aa9e1bfc70d28044524036093e71dc
jakesboy2 <jcwaldrip@att.net>
2018-08-10 15:54:30 -0500

    added unimplemented function deadFlagCleaning to memory manager
    need to find out where to implement this and also test it to see if it actually works lol

--- memory_management.js
+++ memory_management.js
@@ -1,43 +1,71 @@
 module.exports = {
 
 	  //creates function to clear up memory
-      garbageCollection: function()
-      {
+	garbageCollection: function()
+    {
 
-          //loop through creep names
-          for (let name in Memory.creeps) 
-          {
-               //and check if the creep is still alive
-               if (Game.creeps[name] == undefined) 
-               {
-                  //if not, delete the memory entry
-                  delete Memory.creeps[name];
-               }
-          }
-
-		  //loop over rooms
-		  for(let room in Memory.rooms)
-		  {
-		  	  //and check if room exists
-			  if(Game.rooms[room] == undefined)
-			  {
+	  	//loop through creep names
+	 	for (let name in Memory.creeps) 
+		{
+			 //and check if the creep is still alive
+			 if (Game.creeps[name] == undefined) 
+			 {
 				//if not, delete the memory entry
-				 delete Memory.rooms[room];
+				delete Memory.creeps[name];
+			 }
+		}
 
-			  }
-			  else{
-			      //if it exists, delete stale data
-			      Memory.rooms[room].jobQueues = {};
-			  }
-		  }
-		  
-      },
-      //--------------
+		//loop over rooms
+		for(let room in Memory.rooms)
+		{
+			//and check if room exists
+			if(Game.rooms[room] == undefined)
+			{
+				//if not, delete the memory entry
+				delete Memory.rooms[room];
+
+			}
+			else
+			{
+				//if it exists, delete stale data
+				Memory.rooms[room].jobQueues = {};
+			}
+		}
+
+    },
+    //--------------
 	
     deadFlagCleaning: function()
     {
     	//check for any remote, claim, attack, etc rooms that do not have a live flag
-	//delete this rooms from the main room's memory
-    }
-    //------------------
-};
+		//delete this rooms from the main room's memory
+		var ownedRooms = _.filter(Game.rooms, r => r.isOwnedRoom() == true);
+		let currentRoom;
+		let currentRemoteRoom;
+		
+		//get all flags of each type
+		var flags = Game.flags;
+		var remoteFlags = _.filter(Game.flags, f => f.color === COLOR_YELLOW && f.secondaryColor === COLOR_YELLOW);
+		var claimFlags = _.filter(Game.flags, f => f.color === COLOR_WHITE && f.secondaryColor === COLOR_WHITE);
+		var attackFlags = _.filter(Game.flags, f => f.color === COLOR_RED && f.secondaryColor === COLOR_RED);
+
+		//loop over remote rooms for each room and remove any that does not have a live remote flag
+		for(let room in ownedRooms)
+		{
+			currentRoom = ownedRooms[room];
+			
+			//loop over remote rooms for this specific room
+			for(let remoteRoom in currentRoom.memory.remoteRooms)
+			{
+				currentRemoteRoom = currentRoom.memory.remoteRooms[remoteRoom];
+				
+				//check if this assigned remote room has a live flag, if not, delete it
+				if(!_.some(remoteFlags, rf => rf.pos.roomName === currentRemoteRoom["name"]))
+				{
+					delete currentRemoteRoom;
+				}
+			}
+		}
+		
+	};
+	//------------
