e0764e512688ff07ba260475b7b1bc5cbb07b020
jakesboy2 <jcwaldrip@att.net>
2018-08-18 01:40:58 -0500

    testtest

---
+++ role.RemoteDefender.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+module.exports = {
+    run: function(creep) {
+
+		//check the state and act appropirately
+		switch(creep.memory.state)
+		{
+			case 'STATE_SPAWNING':
+
+                creep.runSpawningMilitary();
+
+			break;
+
+
+			case 'STATE_MOVING':
+
+                creep.runMovingMilitary();
+
+			break;
+
+
+		    case 'STATE_ATTACKING':
+
+                creep.runAttackingMilitary();
+
+			break;
+
+            case 'STATE_RANGED_ATTACKING':
+                
+                creep.runRangedAttackingMilitary();
+                
+            break;
+
+            case 'STATE_DEFENDING':
+
+                creep.runDefendingMilitary();
+
+            break;
+
+			default: 
+
+				console.log("Invalid creep state for " + creep.name);
+
+			break;
+		}
+    }
+    //---------------------
+};
